b570417f9790ecde033ddf81c2de1e809f4512f4
Danny <77023480+TheDanDan@users.noreply.github.com>
2024-10-17 08:30:41 -0400

    added new test

--- commands/utility/test.js
+++ commands/utility/test.js
@@ -1,16 +1,35 @@
 const { SlashCommandBuilder } = require('discord.js');
 const wait = require('node:timers/promises').setTimeout;
+const crypto = require("crypto");
+
+const symbols = [":spades:", ":diamonds:", ":clubs:", ":hearts:", ":anchor:", ":crown:"];
+const TEST = 5000;
+const chosen = 0;
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('test')
     .setDescription('Test command'),
   async execute(interaction) {
-    // await interaction.reply("Testing...");
-    // for (let i = 0; i < 10; i++) {
-    //   await wait(1000);
-    //   await interaction.editReply(`Testing... ${i}`);
-    // }
-    interaction.reply(`Websocket heartbeat: ${client.ws.ping}ms.`);
+    var count = 0;
+    const symbolCount = [0, 0, 0, 0, 0, 0];
+    for (let i = 0; i < TEST; i++) {
+      count -= 1;
+
+      const randomSymbols = [0,0,0];
+
+      randomSymbols[0] = Math.floor(crypto.randomBytes(4).readUInt32BE() / 0xFFFFFFFF * (5 + 1));
+      randomSymbols[1] = Math.floor(crypto.randomBytes(4).readUInt32BE() / 0xFFFFFFFF * (5 + 1));
+      randomSymbols[2] = Math.floor(crypto.randomBytes(4).readUInt32BE() / 0xFFFFFFFF * (5 + 1));
+
+      symbolCount[randomSymbols[0]] += 1.1;
+      symbolCount[randomSymbols[1]] += 1.1;
+      symbolCount[randomSymbols[2]] += 1.1;
+
+      if (randomSymbols.includes(chosen)) count++;
+      count += randomSymbols.filter(x => x == chosen).length;
+    }
+    console.log(symbolCount);
+    await interaction.reply(`Result: ${count/TEST} ${symbolCount}`);
   },
 };
